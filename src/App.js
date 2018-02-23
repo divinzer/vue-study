f2ed7933d44c1b1209f861de130df242143d43dd
최세영 <armada.dt@gmail.com>
2018-02-23 17:45:00 +0900

    complete chapter 1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class Developer {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  getName() {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+
+const robin = new Developer('Robin', 'Wieruch');
+console.log(robin.getName());
+
 class App extends Component {
   render() {
-    const helloWorld = '리액트에 오신 여러분을 환영합니다';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+      {list.map(item => {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.auithor}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        )
+      })}
       </div>
     );
   }
